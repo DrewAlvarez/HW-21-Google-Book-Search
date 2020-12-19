8765b2ac6048ee26b725a3a7295c14df6b28cc6a
Drew A <alvarez.drew@gmail.com>
2020-12-19 14:39:49 -0600

    heroku deploy

---
+++ client/build/precache-manifest.3f876be2d979189d77eaf86ec4ebde46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "250300bc3d294389e66f1d1607cdd536",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25a0f1cde0b7f71bbee2",
+    "url": "/static/css/main.29c23a39.chunk.css"
+  },
+  {
+    "revision": "0138ef201bf533297557",
+    "url": "/static/js/2.5e61269b.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.5e61269b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25a0f1cde0b7f71bbee2",
+    "url": "/static/js/main.fffbbf9d.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
